59c937ac28641cd69ed1490deade3cbbe957110e
Rodolfo M. Raya <rmraya@maxprograms.com>
2025-07-23 12:10:07 -0300

    Initial commit

---
+++ ts/constants.ts
@@ -0,0 +1,16 @@
+/*******************************************************************************
+ * Copyright (c) 2025 Maxprograms.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse   License 1.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/org/documents/epl-v10.html
+ *
+ * Contributors:
+ *     Maxprograms - initial API and implementation
+ *******************************************************************************/
+
+export class Constants {
+    public static readonly VERSION: string = "1.0.0";
+    public static readonly BUILD: string = "20250723_1204";
+}
---
+++ ts/hybridtm.ts
@@ -0,0 +1,20 @@
+/*******************************************************************************
+ * Copyright (c) 2025 Maxprograms.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse   License 1.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/org/documents/epl-v10.html
+ *
+ * Contributors:
+ *     Maxprograms - initial API and implementation
+ *******************************************************************************/
+
+import { Constants } from "./constants";
+
+export class HybridTM {
+
+    constructor() {
+        
+    }
+}

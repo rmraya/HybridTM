68832056332f2643945065ba535574a2748fda0f
Rodolfo M. Raya <rmraya@maxprograms.com>
2025-12-08 16:43:18 -0300

    added copyright

--- ts/hybridtmFactory.ts
+++ ts/hybridtmFactory.ts
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2025 Maxprograms.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse   License 1.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/org/documents/epl-v10.html
+ *
+ * Contributors:
+ *     Maxprograms - initial API and implementation
+ *******************************************************************************/
+
 import { promises as fs } from "node:fs";
 import os from "node:os";
 import path from "node:path";
